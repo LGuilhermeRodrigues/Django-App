136efbab434eb13c6c5b1db572aaa5925db5be98
Luis Guilherme Silva Rodrigues <guilherme.rodrigues@unesp.br>
2021-07-06 21:59:56 -0300

    fix

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
 web: gunicorn welcome.wsgi
+DISABLE_COLLECTSTATIC=1
--- welcome/settings.py
+++ welcome/settings.py
@@ -1,6 +1,3 @@
 django_heroku.settings(locals())
 TIME_ZONE = 'America/Sao_Paulo'
 LANGUAGE_CODE = 'pt-BR'
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-DISABLE_COLLECTSTATIC=1
